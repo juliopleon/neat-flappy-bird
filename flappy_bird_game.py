31033420b2e4e8c619a18d2fc8bf285ac97808c2
Julio Leon <juliopena143@icloud.com>
2025-03-10 16:39:00 -0600

    jump and init

--- flappy_bird_game.py
+++ flappy_bird_game.py
@@ -17,3 +17,17 @@ class Bird:
     MAX_ROTATION = 25
     ROT_VEL = 20
     ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+    
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
