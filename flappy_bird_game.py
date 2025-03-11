b84c53c9b3d7d3ab969165da182abecafbfd61b2
Julio Leon <juliopena143@icloud.com>
2025-03-11 14:02:54 -0600

    march 11th minor touches

--- flappy_bird_game.py
+++ flappy_bird_game.py
@@ -62,3 +62,12 @@ class Bird:
             self.img = self.IMGS[1]
         elif self.img_count < self.ANIMATION_TIME*3:
             self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
