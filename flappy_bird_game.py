baf826061b924d251984612f8a6d128d2e90c023
Julio Leon <juliopena143@icloud.com>
2025-03-11 16:02:49 -0600

    drawing bird and window errors

--- flappy_bird_game.py
+++ flappy_bird_game.py
@@ -91,12 +91,15 @@ def draw_window(win, bird):
 
 def main():
     bird = Bird(200,200)
+    win = pygame.display.set_mode(WIN_WIDTH, WIN_HEIGHT)
     run = True
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        draw_window(win, bird)
 
     pygame.quit()
     quit()
